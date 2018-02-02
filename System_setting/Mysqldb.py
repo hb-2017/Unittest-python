29cb57a448740459b399f6829d3aea259ed15863
hb-2017 <819920696@qq.com>
2018-02-02 19:06:43 +0800

    测试更新

--- System_setting/Mysqldb.py
+++ System_setting/Mysqldb.py
@@ -6,7 +6,7 @@
 # @Version : $Id$
 
 import os
-import MySQLdb
+import pymysql
 import traceback
 from System_setting.Logger import Logger
 
@@ -14,22 +14,24 @@ logger = Logger(logger='Mysql').getlog()
 
 class Mysql(object):
 
-    def __init__(self, Mysql_ip, Mysql_name, Mysql_password, db_name):
+    def __init__(self, Mysql_ip,Mysql_name,Mysql_password,db_name):
         try:
             # 打开数据库连接
-            self.db = MySQLdb.connect(Mysql_ip, Mysql_name, Mysql_password, db_name)
+            self.db = pymysql.connect(Mysql_ip, Mysql_name, Mysql_password, db_name)
             # 使用cursor()方法获取操作游标
             self.cursor = self.db.cursor()
             logger.info('数据库%s连接成功...' % db_name)
         except Exception as e:
             logger.error('数据库链接异常,%s' % e)
 
+
     def add(self,sql):
         try:
             # 执行sql并返回执行状态
             sql_satatu = self.cursor.execute(sql)
             self.db.commit()
-            return sql_satatu
+            sql_data = self.cursor.fetchall()
+            return sql_satatu,sql_data
         except BaseException as e :
             logger.error('执行sql:%s 发生异常%s,开始回滚'%(sql,e))
             # 方法二：采用traceback模块查看异常
@@ -50,15 +52,30 @@ class Mysql(object):
             return sql_satatu
         except BaseException as e:
             logger.error('执行sql:%s 发生异常%s,开始回滚' % (sql, e))
-            # 方法二：采用traceback模块查看异常
-            # traceback.print_exc()
-            # 方法三：采用sys模块回溯最后的异常
-            # 输出异常信息
-            # info = sys.exc_info()
-            # print(info[0], ":", info[1])
-
             # 异常则回滚
             self.db.rollback()
 
 
+    def updata(self, sql):
+        try:
+            # 执行sql并返回执行状态
+            sql_satatu = self.cursor.execute(sql)
+            self.db.commit()
+            return sql_satatu
+        except BaseException as e:
+            logger.error('执行sql:%s 发生异常%s,开始回滚' % (sql, e))
+            # 异常则回滚
+            self.db.rollback()
+
+
+    def selete(self, sql):
+        try:
+            # 执行sql并返回执行状态
+            sql_satatu = self.cursor.execute(sql)
+            self.db.commit()
+            return sql_satatu
+        except BaseException as e:
+            logger.error('执行sql:%s 发生异常%s,开始回滚' % (sql, e))
+            # 异常则回滚
+            self.db.rollback()
 
---
+++ System_setting/SMTP.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author  : huxiansheng (you@example.org)
+
+import smtplib
+from email.mime.text import MIMEText
+from email.header import Header
+
+# 第三方 SMTP 服务
+mail_host="smtp.163.com"  #设置服务器
+mail_user="m18664594496@163.com"   #用户名
+mail_pass="wwwhubiao165++"   #口令
+
+sender = 'm18664594496@163.com'
+receivers = '[819920696@qq.com]'  # 接收邮件，可设置为你的QQ邮箱或者其他邮箱
+
+message = MIMEText('Python SMTP 邮件发送测试', 'plain', 'utf-8')
+message['From'] = Header("From测试",'utf-8')
+message['To'] =  Header("To测试", 'utf-8')
+
+subject = 'Python SMTP 邮件测试'
+message['Subject'] = Header(subject, 'utf-8')
+
+# try:
+smtpObj = smtplib.SMTP()
+smtpObj.connect(mail_host, 25)    # 25 为 SMTP 端口号
+smtpObj.login(mail_user,mail_pass)
+smtpObj.sendmail(sender, receivers,
+message.as_string())
+print ("邮件发送成功")
+# except smtplib.SMTPException:
+print ("Error: 无法发送邮件")
+
+
